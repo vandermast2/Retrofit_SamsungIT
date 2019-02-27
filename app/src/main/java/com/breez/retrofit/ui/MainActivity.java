f7fc6c288e8918c7b808e9f44f93f911cfb81715
sergey <sergey.k@adviscent.com>
2019-02-27 16:55:11 +0200

    change recyclerAdapter, add divider

--- app/src/main/java/com/breez/retrofit/ui/MainActivity.java
+++ app/src/main/java/com/breez/retrofit/ui/MainActivity.java
@@ -1,53 +1,57 @@
 package com.breez.retrofit.ui;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.TextView;
 
 import com.breez.retrofit.R;
 import com.breez.retrofit.Service;
 import com.breez.retrofit.model.DataModel;
-import com.breez.retrofit.model.Result;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity implements Callback<DataModel> {
+    RecyclerView recyclerView;
+    TextView url,title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView = findViewById(R.id.rvReceipts);
+        url = findViewById(R.id.txtUrl);
+        title = findViewById(R.id.txtTitle);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(recyclerView.getContext(),
+                ((LinearLayoutManager) layoutManager).getOrientation());
+        recyclerView.addItemDecoration(dividerItemDecoration);
         getRecipe();
     }
 
     private void getRecipe() {
         Call<DataModel> call = Service.getApi().getRecipe();
-        call.enqueue(new Callback<DataModel>() {
-            @Override
-            public void onResponse(Call<DataModel> call, Response<DataModel> response) {
-                response.body().toString();
-            }
-
-            @Override
-            public void onFailure(Call<DataModel> call, Throwable t) {
-                t.getMessage();
-            }
-        });
-    }
-
-
-    @Override
-    public void onResponse(Call call, Response response) {
-        response.body();
+        call.enqueue(this);
     }
 
     @Override
-    public void onFailure(Call call, Throwable t) {
+    public void onResponse(Call<DataModel> call, Response<DataModel> response) {
+        DataModel dataModel = response.body();
+        url.setText(dataModel != null ? dataModel.getHref() : null);
+        title.setText(dataModel.getTitle());
+        RecipeAdapter recipeAdapter = new RecipeAdapter();
+        recipeAdapter.setList(dataModel.getResults());
+        recyclerView.setAdapter(recipeAdapter);
+//        recyclerView.getAdapter().notifyDataSetChanged();
+    }
+
+    @Override
+    public void onFailure(Call<DataModel> call, Throwable t) {
 
     }
 }
--- app/src/main/java/com/breez/retrofit/ui/RecipeAdapter.java
+++ app/src/main/java/com/breez/retrofit/ui/RecipeAdapter.java
@@ -5,14 +5,25 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.breez.retrofit.R;
-import com.breez.retrofit.model.DataModel;
+import com.breez.retrofit.model.Result;
+import com.bumptech.glide.Glide;
 
 import java.util.List;
 
 public class RecipeAdapter extends RecyclerView.Adapter<RecipeAdapter.ViewHolder> {
-    List<DataModel> list;
+    private List<Result> list;
+    public void setList(List<Result> list){
+        this.list =list;
+        notifyDataSetChanged();
+    }
+
+    public RecipeAdapter() {
+    }
+
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
@@ -24,19 +35,32 @@ public class RecipeAdapter extends RecyclerView.Adapter<RecipeAdapter.ViewHolder
         viewHolder.bind(list.get(i));
     }
 
+    public RecipeAdapter(List<Result> list) {
+        this.list = list;
+    }
+
     @Override
     public int getItemCount() {
         return list.size();
     }
 
     class ViewHolder extends RecyclerView.ViewHolder{
+        private TextView txtUrl,txtTitle,txtIngredients;
+        private  ImageView imfPhoto;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            imfPhoto = itemView.findViewById(R.id.imageView);
+            txtIngredients = itemView.findViewById(R.id.txtIngredients);
+            txtTitle = itemView.findViewById(R.id.txtTitle);
+            txtUrl = itemView.findViewById(R.id.txtUrl);
         }
 
-        public void bind(DataModel dataModel) {
-
+        public void bind(Result dataModel) {
+            Glide.with(imfPhoto.getContext()).load(dataModel.getThumbnail()).into(imfPhoto);
+            txtUrl.setText(dataModel.getHref());
+            txtTitle.setText(dataModel.getTitle());
+            txtIngredients.setText(dataModel.getIngredients());
         }
     }
 }
